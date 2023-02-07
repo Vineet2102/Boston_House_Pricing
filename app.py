eed2081a39ae0859102d7be74f7f0149e20bc6ab
vineet <vineetrpanicker2002@gmail.com>
2023-02-07 12:33:04 +0530

    pickle load

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import pickle
 from flask import Flask, request, app, jsonify, url_for, render_template
 
@@ -8,24 +9,28 @@ app = Flask(__name__)
 # load the model
 regmodel = pickle.load(open('regmodel.pkl', 'rb'))
 scalar = pickle.load(open('scaling.pkl', 'rb'))
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
     data = request.json['data']
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
     output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = [float(x) for x in request.form.values()]
-    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
     print(final_input)
     output = regmodel.predict(final_input)[0]
     return render_template("home.html", prediction_text="The House price predicted is {}".format(output))
